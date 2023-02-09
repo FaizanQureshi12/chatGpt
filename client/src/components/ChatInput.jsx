ac37b24015a1b7977c7ba7286b5c1ae2791a284d
Faizan <qfaizan489@gmail.com>
2023-02-09 21:02:35 +0500

    fr-hd last part

---
+++ client/src/components/ChatInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChatInput = () => {
+  return (
+    <div >
+        <textarea />
+        <img src="./send.png" alt="" />
+    </div>
+  )
+}
+
+export default ChatInput
